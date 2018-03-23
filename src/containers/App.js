bdfba6f373f02ff409cf415eb9168e035db8c858
YudhaYuki <Yudha.Yuki@hotmail.com>
2018-03-23 10:58:14 +0100

    Checking whether something changes to minimize the execution workflow

--- src/containers/App.js
+++ src/containers/App.js
@@ -29,7 +29,8 @@ class App extends Component {
 
   shouldComponentUpdate (nextProps, nextState) {
     console.log('[UPDATE App.js] Inside shouldComponentUpdate', nextProps, nextState);
-    return true;
+    return nextState.persons !== this.state.persons ||
+      nextState.showPersons !== this.state.showPersons ;
   }
 
   componentWillUpdate (nextProps, nextState) {
@@ -95,6 +96,7 @@ class App extends Component {
 
     return (
         <div className={classes.App}>
+          <button onClick={() => {this.setState({showPersons: true})}} >Show Persons </button>
           <Cockpit 
             appTitle={this.props.title}
             showPersons={this.state.showPersons} 
